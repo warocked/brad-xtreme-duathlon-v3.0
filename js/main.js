35caafa2f94c86ae35fe5983afd5c0974fda831b
Anthony King <king@ideasmaker.net>
2018-10-18 22:08:35 +0800

    added coutner

--- js/main.js
+++ js/main.js
@@ -19,6 +19,17 @@
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
 
+    var clockx;
+
+    var currentDate = new Date();
+    var futureDate  = new Date('2018-12-09 05:30:00');
+    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+    clockx = $('.clock').FlipClock(diff, {
+        clockFace: 'DailyCounter',
+        countdown: true
+    });
+
 
    /* Preloader
     * -------------------------------------------------- */
@@ -155,7 +166,7 @@
    /* Stat Counter
     * ------------------------------------------------------ */
     var clStatCount = function() {
-        
+        /*
         var statSection = $(".about-stats"),
             stats = $(".stats__count span");
 
@@ -187,6 +198,7 @@
             offset: "90%"
 
         });
+        */
     };
 
 
